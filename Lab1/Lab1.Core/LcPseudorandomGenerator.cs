c8abd1090f97dfe96613abad9c4f82a8658938f7
Sviatoslav Bachynskyi <ba4insckij.s@gmail.com>
2020-10-11 00:59:37 +0300

    [Lab1] check value appearance differently

--- Lab1/Lab1.Core/LcPseudorandomGenerator.cs
+++ Lab1/Lab1.Core/LcPseudorandomGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Lab1.Core
@@ -7,11 +9,21 @@ namespace Lab1.Core
     {
         public static uint Generate(LcConstants constants, StreamWriter streamWriter = null, StringWriter stringWriter = null, uint count = 0)
         {
+            const ulong lastAcceptedValue = 2147483648;
+
             var writers = new List<TextWriter>();
             ulong x0 = constants.X0;
             ulong a = constants.A;
             ulong c = constants.C;
             ulong m = constants.M;
+
+            if (m > lastAcceptedValue)
+            {
+                throw new ArgumentException($"value '{m}' is higher than last accepted value for m - '{lastAcceptedValue}'");
+            }
+
+            var lastPossibleNumberAppeared = false;
+            var numberAppearanceArray = new BitArray((m == lastAcceptedValue) ? (int)(m - 1) : (int)m);
 
             if (stringWriter != null && count != 0)
             {
@@ -32,24 +44,34 @@ namespace Lab1.Core
 
             while (writers.Count != 0)
             {
+                if (writers.Contains(streamWriter))
+                {
+                    var wasAdded = x == lastAcceptedValue - 1 ? lastPossibleNumberAppeared : numberAppearanceArray.Get((int)x);
+                    if (wasAdded)
+                    {
+                        period = i;
+                        writers.Remove(streamWriter);
+                    }
+                    else
+                    {
+                        if (x == lastAcceptedValue)
+                            lastPossibleNumberAppeared = true;
+                        else
+                            numberAppearanceArray.Set((int)x, true);
+                    }
+                }
+                if (i == count && writers.Contains(stringWriter))
+                {
+                    writers.Remove(stringWriter);
+                }
+
                 foreach (var writer in writers)
                 {
                     writer.Write(i == 0 ? $"{x}" : $", {x}");
                 }
 
-                x = ((a  % m * x % m) % m + c % m) % m;
+                x = ((a % m * x % m) % m + c % m) % m;
                 i++;
-
-                if (x == constants.X0 && writers.Contains(streamWriter))
-                {
-                    period = i;
-                    writers.Remove(streamWriter);
-                }
-
-                if (i == count && writers.Contains(stringWriter))
-                {
-                    writers.Remove(stringWriter);
-                }
             }
 
             return period;
