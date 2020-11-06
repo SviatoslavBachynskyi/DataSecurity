452cf66654b6642d84e92856498cf2c48e581850
Sviatoslav Bachynskyi <ba4insckij.s@gmail.com>
2020-11-06 09:53:41 +0200

    [Lab2] created solution, added tests from the book

---
+++ Lab2/Lab2.Core/Md5HashGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2.Core
+{
+    public class Md5HashGenerator
+    {
+        public string Hash(string textInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lab2/Lab2.Test/Md5HashGeneratorTest.cs
@@ -0,0 +1,30 @@
+using Lab2.Core;
+using Xunit;
+
+namespace Lab2.Test
+{
+    public class Md5HashGeneratorTest
+    {
+        private Md5HashGenerator _generator;
+
+        public Md5HashGeneratorTest()
+        {
+            _generator = new Md5HashGenerator();
+        }
+
+        [Theory]
+        [InlineData("", "D41D8CD98F00B204E9800998ECF8427E")]
+        [InlineData("a", "0CC175B9C0F1B6A831C399E269772661")]
+        [InlineData("abc", "900150983CD24FB0D6963F7D28E17F72")]
+        [InlineData("message digest", "F96B697D7CB7938D525A2F31AAF161D0")]
+        [InlineData("abcdefghijklmnopqrstuvwxyz", "C3FCD3D76192E4007DFB496CCA67E13B")]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "D174AB98D277D9F5A5611C2C9F419D9F")]
+        [InlineData("12345678901234567890123456789012345678901234567890123456789012345678901234567890", "57EDF4A22BE3C955AC49DA2E2107B67A")]
+        public void Hash_Positive_ReturnsHash(string input, string expected)
+        {
+            var actual = _generator.Hash(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
