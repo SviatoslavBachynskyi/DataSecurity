42712ddf9b63f3255e3d4b72fd483b4919482493
Sviatoslav Bachynskyi <ba4insckij.s@gmail.com>
2020-11-07 14:02:44 +0200

    [Lab2] changed interfaces to accept both Binary and string data

--- Lab2/Lab2.Core/Md5HashGenerator.cs
+++ Lab2/Lab2.Core/Md5HashGenerator.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Lab2.Core
 {
     public class Md5HashGenerator
     {
-        public string Hash(string textInput)
+        public string Hash(BinaryReader binaryReader)
         {
             throw new NotImplementedException();
+        }
+
+        public string Hash(string textInput)
+        {
+            var binaryReader = new BinaryReader(
+                new MemoryStream(Encoding.UTF8.GetBytes(textInput ?? ""))
+                );
+
+            return Hash(binaryReader);
         }
     }
 }
